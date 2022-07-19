397592d57f4895733a59e1a1cbfb71bd0ea476bc
e-henry <e-henry@users.noreply.github.com>
2022-07-19 14:35:37 +0200

    Ajout des TSV propres aux établissements

--- db/init/60-enrich-columns.sh
+++ db/init/60-enrich-columns.sh
@@ -5,22 +5,27 @@ psql -U $POSTGRES_USER -d $POSTGRES_DB -c "ALTER TABLE siren ADD COLUMN tsv_nomp
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "ALTER TABLE siren ADD COLUMN tsv_enseigne tsvector;"
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "ALTER TABLE siren ADD COLUMN tsv_adresse tsvector;"
 
+# Récupération de toutes les enseignes
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "UPDATE siren S SET enseignes = (SELECT STRING_AGG(enseignes,', ') AS enseignes FROM (select DISTINCT ST.enseigne1etablissement as enseignes from siret ST where ST.siren = S.siren AND ST.enseigne1etablissement IS NOT NULL) tbl);"
 
+# TSV nom entreprise
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "UPDATE siren S
 SET tsv_nomentreprise = 
 setweight(to_tsvector(coalesce(sigleUniteLegale,'')), 'A') || 
 setweight(to_tsvector(coalesce(denominationUniteLegale,'')), 'B');"
 
+# TSV nom prenom
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "UPDATE siren S
 SET tsv_nomprenom = 
 setweight(to_tsvector(coalesce(nomUniteLegale,'')), 'A') || 
 setweight(to_tsvector(coalesce(prenom1UniteLegale,'')), 'A');"
 
+# TSV des enseignes
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "UPDATE siren
 SET tsv_enseigne = 
 setweight(to_tsvector(coalesce(enseignes,'')), 'A');"
 
+# TSV des adresses
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "UPDATE siren S
 SET tsv_adresse = 
 setweight(to_tsvector(coalesce(intermediary_view.numero_voie,'')), 'A') ||
@@ -31,6 +36,7 @@ setweight(to_tsvector(coalesce(intermediary_view.code_postal,'')), 'A') ||
 setweight(to_tsvector(coalesce(intermediary_view.libelle_commune,'')), 'A')
 FROM intermediary_view  WHERE S.siren = intermediary_view.siren;"
 
+# TSV complet
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "UPDATE siren S
 
 SET tsv = 
@@ -87,3 +93,50 @@ psql -U $POSTGRES_USER -d $POSTGRES_DB -c "ALTER TABLE siren ADD COLUMN numero_t
 
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "UPDATE siren S SET numero_tva_intra = (SELECT CASE WHEN tvanumber < 10 THEN concat('FR0',tvanumber,siren) ELSE CONCAT('FR',tvanumber,siren) END FROM (SELECT (12+(3*bigintsiren)%97)%97 as tvanumber, siren FROM (select CAST (siren as BIGINT) as bigintsiren,siren from siren WHERE siren = S.siren) tbl) tbl2);"
 
+#
+# Siret TSV
+#
+
+# TSV des adresses
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c "ALTER TABLE siret ADD COLUMN tsv tsvector;"
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c "ALTER TABLE siret ADD COLUMN tsv_adresse tsvector;"
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c "UPDATE siret S
+SET tsv_adresse = 
+setweight(to_tsvector(coalesce(numerovoieetablissement,'')), 'A') ||
+setweight(to_tsvector(coalesce(indicerepetitionetablissement,'')), 'A') ||
+setweight(to_tsvector(coalesce(typevoieetablissement,'')), 'A') ||
+setweight(to_tsvector(coalesce(libellevoieetablissement,'')), 'A') ||
+setweight(to_tsvector(coalesce(codepostaletablissement,'')), 'A') ||
+setweight(to_tsvector(coalesce(libellecommuneetablissement,'')), 'A') ||
+setweight(to_tsvector(coalesce(libellecommuneetrangeretablissement,'')), 'A') ||
+setweight(to_tsvector(coalesce(distributionspecialeetablissement,'')), 'A') ||
+setweight(to_tsvector(coalesce(codecommuneetablissement,'')), 'A') ||
+setweight(to_tsvector(coalesce(codecedexetablissement,'')), 'A') ||
+setweight(to_tsvector(coalesce(libellecedexetablissement,'')), 'A')
+;"
+
+# TSV général : mix entre siren et adresse de l'établissement
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c "UPDATE siret T
+
+SET tsv = 
+setweight(to_tsvector(coalesce(N.sigleUniteLegale,'')), 'A') || 
+setweight(to_tsvector(coalesce(N.denominationUniteLegale,'')), 'B') || 
+setweight(to_tsvector(coalesce(N.enseignes,'')), 'C') || 
+setweight(to_tsvector(coalesce(N.nomUniteLegale,'')), 'D') || 
+setweight(to_tsvector(coalesce(N.prenom1UniteLegale,'')), 'D') ||
+
+
+setweight(to_tsvector(coalesce(T.numerovoieetablissement,'')), 'D') ||
+setweight(to_tsvector(coalesce(T.indicerepetitionetablissement,'')), 'D') ||
+setweight(to_tsvector(coalesce(T.typevoieetablissement,'')), 'D') ||
+setweight(to_tsvector(coalesce(T.libellevoieetablissement,'')), 'D') ||
+setweight(to_tsvector(coalesce(T.codepostaletablissement,'')), 'D') ||
+setweight(to_tsvector(coalesce(T.libellecommuneetablissement,'')), 'D') ||
+setweight(to_tsvector(coalesce(T.libellecommuneetrangeretablissement,'')), 'D') ||
+setweight(to_tsvector(coalesce(T.distributionspecialeetablissement,'')), 'D') ||
+setweight(to_tsvector(coalesce(T.codecommuneetablissement,'')), 'D') ||
+setweight(to_tsvector(coalesce(T.codecedexetablissement,'')), 'D') ||
+setweight(to_tsvector(coalesce(T.libellecedexetablissement,'')), 'D')
+
+FROM siren N 
+WHERE N.siren=T.siren;"
